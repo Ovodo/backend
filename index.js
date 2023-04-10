b7a970818234b91fdd5cb441a43d24d7e8a73a46
Ovodo <ovdizzle@gmail.com>
2023-04-10 22:16:46 +0100

    lets see

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("this is awesome");
+});
+
+app.listen(port, () =>
+  console.log(
+    `Your Server don start to dey run on port: ${port}  oya start work!!!1`
+  )
+);
